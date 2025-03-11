ed123397ef1c9ea883304906a818c9ce432eed08
Devendra Desktop Win 11 <24f2002329>
2025-03-11 07:59:59 +0530

    Till ChaiAurDjango Lec -2

--- chaiaurDjango/chaiaurDjango/urls.py
+++ chaiaurDjango/chaiaurDjango/urls.py
@@ -12,16 +12,16 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))     # the '/' is important!!
 """
 
-from django.contrib import admin
-from django.urls import path
-from . import views
+from django.contrib import admin     # importing admin from django.contrib  
+from django.urls import path        # importing path from django.urls
+from . import views                 # importing views from .
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('', views.index , name='index'),
-    path('about/', views.about , name='about'),
-    path('contact/', views.contact , name='contact'),
+    path("admin/", admin.site.urls),    # importing admin from django.contrib
+    path('', views.index , name='index'),    
+    path('about/', views.about , name='about'),   
+    path('contact/', views.contact , name='contact'),    
 ]
--- chaiaurDjango/chaiaurDjango/views.py
+++ chaiaurDjango/chaiaurDjango/views.py
@@ -1,12 +1,12 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render     # importing request and render from django.shortcuts
 
 def index(request):
     # return HttpResponse("Hello World")
-    return render(request, 'website/index.html')
+    return render(request, 'website/index.html')    # importing request and render from django.shortcuts
 
 def about(request):
-    return render(request, 'website/about.html')
+    return render(request, 'website/about.html')   
 
 def contact(request):
     return render(request, 'website/contact.html')
