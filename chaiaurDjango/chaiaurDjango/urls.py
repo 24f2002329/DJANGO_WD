58fc795c1a4d48df5f228cdbaa348480a9f4eaa9
Devendra Linux <24f2002329@ds.study.iitm.ac.in>
2025-03-11 15:39:59 +0530

    chaiaurDjango Lec - 5 Added Tailwind and Created Superuser

--- chaiaurDjango/chaiaurDjango/urls.py
+++ chaiaurDjango/chaiaurDjango/urls.py
@@ -25,4 +25,8 @@ urlpatterns = [
     path('about/', views.about , name='about'),   
     path('contact/', views.contact , name='contact'), 
     path('chai/', include('chai.urls')),    # importing chai from chai.urls
+
+
+
+    path("__reload__/", include("django_browser_reload.urls")),    # Always put at last in the urlpatterns
 ]
